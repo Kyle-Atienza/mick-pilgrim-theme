708cd7d18e40657b19e088bc39501afde0f7e752
Kyle Atienza <kylefrancisedison@gmail.com>
2026-09-06 21:24:04 +0800

    yt embed

--- assets/js/socialSharePost.js
+++ assets/js/socialSharePost.js
@@ -2,15 +2,15 @@ const shareUrl = window.location.href;
 const socialShareLinks = document.querySelectorAll(".social-share-link");
 const socialShareDialog = document.querySelector(".social-share-dialog");
 
-socialShareLinks.forEach((link) => {
-    link.addEventListener("click", async (event) => {
-        event.preventDefault();
+// socialShareLinks.forEach((link) => {
+//     link.addEventListener("click", async (event) => {
+//         event.preventDefault();
 
-        if (navigator.share) {
-            await navigator.share({
-                title: document.title,
-                url: shareUrl,
-            });
-        }
-    });
-});
+//         if (navigator.share) {
+//             await navigator.share({
+//                 title: document.title,
+//                 url: shareUrl,
+//             });
+//         }
+//     });
+// });
